30f986f31e3d028eff5bc4539f1ed3c4caca4b70
VictoriavikaA <vikaanufrovich@Vikas-MacBook-Pro.local>
2024-02-12 22:20:45 -0600

    added autouse=True to fixture and base url

--- conftest.py
+++ conftest.py
@@ -2,12 +2,11 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(autouse=True)
 def setting_window_height_width():
+    browser.config.base_url='https://google.com'
     browser.config.window_height = 1024
     browser.config.window_width = 868
+    yield
+    browser.quit()
 
-
-@pytest.fixture(scope='function')
-def open_browser(setting_window_height_width):
-    browser.open('https://google.com')
