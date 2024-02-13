02dd90c4e2d7ca36f70930f8b49973d6f75a87cc
VictoriavikaA <vikaanufrovich@Vikas-MacBook-Pro.local>
2024-02-12 22:22:52 -0600

    changed window width to 768

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selene import browser
 def setting_window_height_width():
     browser.config.base_url='https://google.com'
     browser.config.window_height = 1024
-    browser.config.window_width = 868
+    browser.config.window_width = 768
     yield
     browser.quit()
 
